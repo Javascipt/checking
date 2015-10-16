c1e2d7ca2a5bebeb1704fcfcf0d14b948632944b
Khalid REHIOUI <Rehioui@gmail.com>
2015-10-17 00:54:54 +0100

    Beta version

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # checking
-Checking domain name availability
+
+This package allows you to check the availability of a domain name using the [whoisxmlapi](https://www.whoisxmlapi.com/) api.
+
+### Installation 
+
+```bash
+ $ npm install checking
+```
+
+### How does it work
+
+[whoisxmlapi](https://www.whoisxmlapi.com/) allows you to make free 50 queries, but if you sign up, you can get 500 free queries. You also can buy more balance if you need to.
+
+```javascript
+  var checking = require('checking');
+  
+  /* specifying your whoisxmlapi username and password is optional */
+  checking.username = 'MY USERNAME'
+  checking.password = '******'
+  
+  checking.check('domain.com', function (error, available) {
+    if(error) return console.log(error);
+    if(available) {
+      console.log('domain.com is available');
+    } else {
+      console.log('No chance!');
+    }
+  })
+```
--- check.js
+++ check.js
@@ -1,1 +1,37 @@
-module.exports = {};
+module.exports = (function () {
+
+  var request     = require('request')
+    , toJson      = require('xmljson').to_json
+    , queryString = require('query-string')
+    , apiUrl      = 'https://www.whoisxmlapi.com/whoisserver/WhoisService?'
+  
+  function check (domain, callBack) {
+    var data = {
+        cmd         : 'GET_DN_AVAILABILITY'
+      , domainName  : domain // I need to parse the domain
+      , username    : this.username
+      , password    : this.password
+    }
+
+    request(apiUrl + queryString.stringify(data), function (error, response, body) {
+      if(error) return callBack(error);
+      toJson(body.toString(), function (error, data) {
+        /* handling errors */
+        if(error) return callBack(new TypeError('The whoisxmlapi is down, please try later'));
+        if(data.ErrorMessage) return callBack(new TypeError(data.ErrorMessage.msg));
+
+        /* data returned successfully */
+        callBack(null, data.DomainInfo.domainAvailability === "AVAILABLE");
+      });
+    });
+  }
+
+  return {
+      username  : ''
+    , password  : ''
+    , check     : check.bind(this)
+    , checkAll  : function () {} // bulk availability checking
+    , balance   : function () {} // returning whoisxmlapi balance of the username
+  }
+  
+}());
